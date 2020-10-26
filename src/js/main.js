4441b7a24c4e8ef40da8435d67ae1c1bdd46933e
Adam Bergman <adam@fransvilhelm.com>
2020-10-26 14:02:30 +0100

    Remove traces of specs

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ import 'zenscroll';
 import balanceText from 'balance-text';
 import './lib/navigation';
 import './lib/style-phone';
-import './lib/toggle-specs';
 import './lib/animate-logo';
 
 balanceText('.section__content h1');
